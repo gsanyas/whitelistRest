89565f4a4f87b9c8e2545227e05af004b2d3ce8f
gsanyas <guillem.sanyas@imt-atlantique.net>
2020-11-26 12:16:50 +0100

    changed database config

--- Model.js
+++ Model.js
@@ -2,7 +2,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 var exports = module.exports = {};
 // Database connexion and sync
 
-const sequelize = new Sequelize('whitelist', 'root', '', { // database, username, password
+const sequelize = new Sequelize('whitelist', 'back', 'imta2020', { // database, username, password
     host: 'localhost',
     dialect: 'mariadb'
 }) //sequelize is an instance of connexion, while Sequelize is the whole library
