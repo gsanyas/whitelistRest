f0fc6d806dbeddcbeced7e3337277d7c89bc9a27
agustin <aguszorza@gmail.com>
2020-12-03 01:07:52 +0100

    captcha endpoint

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+
 const { checkToken } = require('./utils/checktoken');
 const { loginController, isConnected } = require('./controllers/loginController');
 const { getEmail, checkEmail, deleteEmail, restoreEmail } = require('./controllers/emailController');
 const { getUser } = require('./controllers/userController');
+const { verifyEmail } = require('./controllers/captchaController');
 
 // Config
 const port = 8070;
-const origin = 'http://localhost:8080';
+const origin = 'http://localhost:4200';
 
 // Express routing
 const app = express()
@@ -17,6 +19,7 @@ const app = express()
 app.use(cors({ credentials: true, origin: origin }))
 app.use(cookieParser())
 app.use(express.json())
+
 
 // listen to incoming requests on port 8000
 app.listen(port, () => {
@@ -31,3 +34,4 @@ app.get('/api/emails', checkToken, getEmail);
 app.get('/api/user', checkToken, getUser);
 app.delete('/api/emails', checkToken, checkEmail, deleteEmail);
 app.put('/api/emails/restore', checkToken, checkEmail, restoreEmail)
+app.put('/api/verify/:id', verifyEmail)
