5bf5bd63c4ca233088a2f084ae0ec197bdfeeb51
agustin <aguszorza@gmail.com>
2020-12-07 19:34:47 +0100

    register user

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -2,17 +2,22 @@ require('dotenv').config();
 const crypto = require("crypto")
 const fs = require('fs')
 
+const {User} = require('../models/user')
+
+
 
 // TODO (possible): check the connection with the IMAP server (before the register in another endpoint)
 exports.register = async (req, res) => {
     // TODO: add all the register logic
+    const email = req.body.email;
+    const fullName = req.body.full_name;
+    const password = req.body.password;
 
     // encryption logic
     // TODO: get the other data
-    const public_key = process.env.PUBLIC_KEY_PATH;
-    const password = crypto.publicEncrypt(
-        {
-            key: fs.readFileSync(public_key, 'utf8'),
+    const publicKey = process.env.PUBLIC_KEY_PATH;
+    const emailPassword = crypto.publicEncrypt({
+            key: fs.readFileSync(publicKey, 'utf8'),
             padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
             oaepHash: "sha256",
         },
@@ -20,8 +25,13 @@ exports.register = async (req, res) => {
         Buffer.from(req.body.email_password)
     )
 
-    console.log("encrypted data: ", password.toString("base64"));
-    // TODO: save the user in the database.
-
-    res.status(204).send("");
+    User.build({ email: email, full_name: fullName, password: password, email_password: emailPassword.toString("base64") }).save().then(user => {
+        if (user) {
+            res.status(204).send("");
+        } else {
+            res.status(404).send("Register Error");
+        }
+    }).catch(error => {
+        res.status(404).send("Register Error");
+    })
 }
