ca5da5199421908dd240673b334700845ad40b0a
gsanyas <guillem.sanyas@imt-atlantique.net>
2020-12-16 14:51:11 +0100

    escape special chars

--- controllers/expressionController.js
+++ controllers/expressionController.js
@@ -1,10 +1,19 @@
+replaceAllRegexSpecial = str => {
+    var mystr = str
+    const regexSpecial = "?[$#+-|'.,!^()]/{}=<>\\"
+    regexSpecial.split().forEach(c => {
+        mystr = mystr.replace(c,"\\"+c)
+    })
+    return mystr
+}
+
 exports.addRegular = (list,regularList) => async (req,res) => {
     const userId = req.user // obtained from filter
     const expression = req.body.expression
     if (expression === null) res.status(404).send('Expression required')
     if (! expression.match(/.*@.*/)) res.sendStatus(422)
     if (expression.match(/.*\*.*/)) {
-        const regex = expression.replace(/\*/g, ".*").replace(/\./g,"\.")
+        const regex = replaceAllRegexSpecial(expression.replace(/\*/g, ".*"))
         try {
             await regularList.create({
                 user_expression: expression,
