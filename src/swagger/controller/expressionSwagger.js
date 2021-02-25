a0c96051292a84c8f505dd740d2be81f292ccb1f
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-02-25 19:57:08 +0100

    docs: swagger getExpression

--- src/swagger/controller/expressionSwagger.js
+++ src/swagger/controller/expressionSwagger.js
@@ -5,13 +5,14 @@ const {
 const { swaggerJsonContent, swaggerErrorContent } = require('../utilSwagger')
 const { RegexSwagger } = require('../model/regexSwagger')
 const { badBodyErrorSwagger } = require('../filter/checkBodySwagger')
+const { checkTokenErrorSwagger } = require('../filter/checkTokenSwagger')
 
 const expressionBodyExample = {
     expression: 'user+*@gmail.com'
 }
 
 /**
- * OpenAPI description of the route
+ * OpenAPI description of the addExpression routes
  * - it is given a path in the swagger/route directory
  * - depending on isWhite, it describes the /whitelist or the /blacklist route
  * @param {boolean} isWhite
@@ -36,6 +37,33 @@ exports.addExpressionSwagger = isWhite => {
                 400: swaggerErrorContent(addExpressionInvalidBody(color)),
                 401: checkTokenErrorSwagger,
                 404: badBodyErrorSwagger(`/list/${color}list/add-body`, expressionBodyPrototype)
+            }
+        }
+    }
+}
+
+/**
+ * OpenAPI description of the getExpression routes
+ * - it is given a path in the swagger/route directory
+ * - depending on isWhite, it describes the /whitelist or the /blacklist route
+ * @param {boolean} isWhite
+ */
+exports.getExpressionSwagger = isWhite => {
+    const color = isWhite ? 'white' : 'black'
+    return {
+        get: {
+            tags: ['List', 'Expression'],
+            summary: 'Add ',
+            security: [{ cookieAuth: [] }],
+            responses: {
+                200: {
+                    description: `The list of expressions and emails in the ${color}list`,
+                    content: swaggerJsonContent({ type: 'string[]' }, [
+                        'user@gmail.com',
+                        'user+*@gmail.com'
+                    ])
+                },
+                401: checkTokenErrorSwagger
             }
         }
     }
